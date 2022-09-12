29a28644b3573369a638b991bf56a8dea080480d
Magister-pilat <pilat0504@mail.ru>
2022-09-13 00:29:39 +0300

    LR1 обработка оператора or

--- LR1/my_function.py
+++ LR1/my_function.py
@@ -1,4 +1,10 @@
 
 def interim_results(facts, rules):
     interim_results = []
-    
+    for i in rules:
+         for j in i['if']:
+              if j == 'or':
+                  for a in facts:
+                       if a in i['if'][j]:
+                           interim_results.append
+                          
