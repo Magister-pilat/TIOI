fed740b5bb894eace87fcf9f51193c8cf76bf8ca
Magister-pilat <pilat0504@mail.ru>
2022-09-18 19:44:06 +0300

    LR1 решил сделать изменения в interim_results()

--- LR1/my_function.py
+++ LR1/my_function.py
@@ -4,23 +4,23 @@ def interim_results(facts, rules):
     for i in rules:
          for j in i['if']:
               if j == 'or':
-                  for a in facts:
-                       if a in i['if'][j]:
+                  for a in i['if'][j]:
+                       if a in facts:
                            interim_results.append({facts:i['then']})
                            break
               if j == 'and':
                   count = len(i['if'][j])
                   counter = 0
-                  for a in facts:
-                       if a in i['if'][j]:     
+                  for a in i['if'][j]:
+                       if a in facts:     
                            counter = counter +1
                   if counter == count:
                            interim_results.append({facts:i['then']})
               if j == 'not':
                   count = len(i['if'][j])
                   counter = 0
-                  for a in facts:
-                       if a not in i['if'][j]:     
+                  for a in i['if'][j]:
+                       if a not in facts:     
                            counter = counter +1
                   if counter == count:
                            interim_results.append({facts:i['then']})
