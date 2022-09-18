99b076fb14d8a84f8a43beade9d355b654da6a69
Magister-pilat <pilat0504@mail.ru>
2022-09-19 01:49:53 +0300

    LR1 добавил новую функцию results(), которая является улучшенной версией interim_results()

--- LR1/my_function.py
+++ LR1/my_function.py
@@ -29,4 +29,78 @@ def interim_results(facts, rules):
                   if counter == count:
                            interim_results.append([facts,i['then']])
               
-    return interim_results      
+    return interim_results 
+
+def results(facts, rules):
+    fact = set(facts)
+    interim_results = []
+    for i in rules:
+         for j in i['if']:
+              if j == 'or':
+                  for a in i['if'][j]:
+                       #if a in facts:
+                       if a in fact:
+                           #interim_results.append([facts,i['then']])
+                           if len(interim_results) == 0:
+                           		interim_results.append({'if': facts, 'or': i['if'][j], 'then': i['then']})
+                           		break
+                           else:
+                                	put = True
+                                	for m in interim_results:
+                                        	if 'or' in m:
+                                        		if (m['or'] == i['if'][j] and m['then'] != i['then']) or ( m['or'] != i['if'][j] and m['then'] == i['then']):
+                                                		put = False
+                                	                	break
+                                        	if 'and' in m:
+                                                	if (m['and'] == i['if'][j]) and (m['then'] != i['then']):
+                                                        	put = False
+                                                        	break
+                                	if put == True:
+                                		interim_results.append({'if': facts, 'or': i['if'][j], 'then': i['then']})
+              if j == 'and':
+                  count = len(i['if'][j])
+                  counter = 0
+                  for a in i['if'][j]:
+                       #if a in facts:
+                       if a in fact:     
+                           counter = counter +1
+                       else:
+                           break
+                  if counter == count:
+                           #interim_results.append([facts,i['then']])
+                           if len(interim_results) == 0:
+                           	interim_results.append({'if': facts, 'and': i['if'][j], 'then': i['then']})
+                           else:
+                                put = True
+                                for m in interim_results:
+                                        if 'and' in m:
+                                                if (m['and'] == i['if'][j] and m['then'] != i['then']) or ( m['and'] != i['if'][j] and m['then'] == i['then']):
+                                                	put = False
+                                	                break
+                                if put == True:
+                                	interim_results.append({'if': facts, 'and': i['if'][j], 'then': i['then']})
+                                          
+              if j == 'not':
+                  count = len(i['if'][j])
+                  counter = 0
+                  for a in i['if'][j]:
+                       #if a not in facts:
+                       if a not in fact:     
+                           counter = counter +1
+                       else:
+                           break
+                  if counter == count:
+                           #interim_results.append([facts,i['then']])
+                           if len(interim_results) == 0:
+                                 interim_results.append({'if': facts, 'not': i['if'][j], 'then': i['then']})
+                           else:
+                                 put = True
+                                 for m in interim_results:
+                                         if 'not' in m:
+                                                if (m['not'] == i['if'][j] and m['then'] != i['then']) or ( m['not'] != i['if'][j] and m['then'] == i['then']):
+                                                	put = False
+                                	                break
+                                 if put == True:
+                                          interim_results.append({'if': facts, 'not': i['if'][j], 'then': i['then']}) 
+              
+    return interim_results     
