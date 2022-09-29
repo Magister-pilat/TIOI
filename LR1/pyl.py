cc3d98646851c07336c42a6bd0960877cfab5d37
Magister-pilat <pilat0504@mail.ru>
2022-09-29 23:47:36 +0300

    LR1 подправил содержимое файла pyl.py

--- LR1/pyl.py
+++ LR1/pyl.py
@@ -1,38 +1,6 @@
 from random import choice, shuffle, randint
 from time import time
 
-def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-	rules = []
-	for j in range(0, n_generate):
-
-	    log_oper = choice(log_oper_choice)  #not means and-not (neither)
-	    if n_max < 2:
-		    n_max = 2
-	    n_items = randint(2,n_max)
-	    items = []
-	    for i in range(0,n_items):
-		    items.append( randint(1,code_max) )
-	    rule = {
-	          'if':{
-	              log_oper:	 items         
-	           },
-	           'then':code_max+j
-	        }
-	    rules.append(rule)
-	shuffle(rules)
-	return(rules)
-
-def generate_seq_facts(M):
-	facts = list(range(0,M))
-	shuffle(facts)
-	return facts
-
-def generate_rand_facts(code_max, M):
-	facts = []
-	for i in range(0,M):
-		facts.append( randint(0, code_max) )
-	return facts
-
 def results(facts, rules):
     fact = set(facts)
     interim_results = []
@@ -107,15 +75,4 @@ def results(facts, rules):
               
     return interim_results 
 
-time_start = time()
-#N = 100000
-#M = 1000
-rules = generate_simple_rules(100, 4, 10000)
-facts = generate_rand_facts(100, 1000)
-print("%d rules generated in %f seconds" % (1000,time()-time_start))
 
-time_start = time()
-in_res = results(facts, rules)
-rez = time()-time_start
-print(rez)
-print("%d facts validated vs %d rules in %f seconds" % (1000,10000,rez))
